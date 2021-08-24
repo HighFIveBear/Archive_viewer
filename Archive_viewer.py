1e110add4432afaa2655a0d2070765dd2782efc6
HighFiveBear <intmindresp@gmail.com>
2021-08-24 23:14:24 +0300

    Added: 1)ability to open SVBU files 2)button and func to save figures of all params 3)ability to open multiple files. Cleaned code, to match PEP8 conventions

--- Archive_viewer.py
+++ Archive_viewer.py
@@ -1,44 +1,127 @@
 import tkinter as tk
 from tkinter import filedialog, Menu, ttk
 import csv
-import pandas as pd
+import openpyxl
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+import os
 
-file_name = ''
+# file_name = ''
 header_row = []
 kks_code ='Время'
 
 
 def file_converter():
 
-    file_name = filedialog.askopenfilename()
+    files = filedialog.askopenfilenames(filetypes = [('text files','.txt')])
 
-    with open(file_name, 'r', encoding = 'windows-1251') as f:
-        lines = f.readlines()
+    if len(files) == 0:
 
-    del lines[:4]
-    file_name = file_name.replace('.txt','.csv')
+        return None
 
-    with open(file_name,'w+') as f:
-        for line in lines:
-            line = line.replace('|',',')
-            f.write(line)
+    save_csv = tk.messagebox.askyesno('Сообщение', 
+                        'Сохранить файлы в формате .csv?')
 
-    save_ex = tk.messagebox.askyesno('Сообщение', 'Требуется ли дополнительно сохранить файл в Excel?')
+    if save_csv:
+        for file in files:
+            csv_converter(file)
+
+
+    save_ex = tk.messagebox.askyesno('Сообщение', 
+                        'Cохранить файлы в формате .xlsx?')
 
     if save_ex:
-        read_file = pd.read_csv(file_name, encoding = 'windows-1251')
-        read_file.to_excel(file_name.replace('.csv','.xlsx'), index = None, header = True)
+        for file in files:
+            xlsx_converter(file)
 
-    tk.messagebox.showinfo('Сообщение', 'Файл ' + file_name + ' успешно конвертирован!')
+    if (not save_csv) & (not save_ex):
+
+        tk.messagebox.showinfo('Вопрос', 'Зачем было сюда жать?')
+
+    else:
+
+        tk.messagebox.showinfo('Сообщение', 'Файлы успешно конвертированы!')
+
+
+def csv_converter(file_name):
+
+    try:
+
+        with open(file_name, 'r', encoding = 'windows-1251') as f:
+            lines = f.readlines()
+
+            del lines[:4]
+            file_name = file_name.replace('.txt','.csv')
+
+            with open(file_name,'w+') as f:
+                for line in lines:
+                    line = line.replace('|',',')
+                    f.write(line)
+                
+    except UnicodeDecodeError:
+
+        with open(file_name, 'r', encoding = 'utf-8') as f:
+            lines = f.readlines()
+            last_line = lines[-1]
+
+            garbage = 1 + len(last_line.split('\t'))
+
+            del lines[:garbage]
+            file_name = file_name.replace('.txt','.csv')
+
+            with open(file_name,'w+') as f:
+                for line in lines:
+                    line = line.replace('\t',',')
+                    f.write(line)
+
+
+def xlsx_converter(file_name):
+
+    wb = openpyxl.Workbook()
+    ws = wb.active
+
+    try:
+
+        with open(file_name, 'r', encoding = 'windows-1251') as f:
+            lines = f.readlines()
+
+            del lines[:4]
+            file_name = file_name.replace('.txt','.csv')
+
+            with open(file_name,'w+') as f:
+                for line in lines:
+                    line = line.replace('|',',')
+                    f.write(line)
+                
+    except UnicodeDecodeError:
+
+        with open(file_name, 'r', encoding = 'utf-8') as f:
+            lines = f.readlines()
+            last_line = lines[-1]
+
+            garbage = 1 + len(last_line.split('\t'))
+
+            del lines[:garbage]
+            file_name = file_name.replace('.txt','.csv')
+
+            with open(file_name,'w+') as f:
+                for line in lines:
+                    line = line.replace('\t',',')
+                    f.write(line)
+
+    with open(file_name) as f:
+        reader = csv.reader(f, delimiter = ',')
+        for row in reader:
+            ws.append(row)
+
+    wb.save(file_name.replace('.csv','.xlsx'))
 
 
 def file_opener():
 
     global header_row, file_name
 
-    file_name = filedialog.askopenfilename()
+    file_name = filedialog.askopenfilename(filetypes = [('CSV files','.csv')])
 
     with open(file_name) as f:
         reader = csv.reader(f)
@@ -47,25 +130,34 @@ def file_opener():
         dates = []
 
         for row in reader:
-            current_date = datetime.strptime(row[0], '%d.%m.%Y %H:%M:%S.%f ')
+            try:
+                current_date = datetime.strptime(row[0], 
+                                '%d.%m.%Y %H:%M:%S.%f ')
+            except ValueError:
+                current_date = datetime.strptime(row[0], '%d.%m.%y %H:%M:%S')
             dates.append(current_date)
 
-    name = tk.Label(window, anchor ='nw' ,text = f"Открыт файл: {file_name}" )
+    name = tk.Label(window, anchor ='nw' ,text = f'Открыт файл: {file_name}')
     name.grid(column = 0, row = 0, sticky = 'nw')
 
-    cont = tk.Label(window, anchor = 'nw', text = f"Число параметров: {len(header_row) - 2}" )
+    cont = tk.Label(window, anchor = 'nw', 
+                        text = f'Число параметров: {len(header_row) - 2}')
     cont.grid(column = 0, row = 1, sticky = 'nw')
 
-    time_period = tk.Label(window, anchor = 'nw', text = f"Период времени: с {dates[0].strftime('%d.%m.%Y %H:%M:%S')} по {dates[-1].strftime('%d.%m.%Y %H:%M:%S')}" )
+    time_period = tk.Label(window, anchor = 'nw', text = 'Период времени: с '
+                    f'{dates[0].strftime("%d.%m.%Y %H:%M:%S")} по ' 
+                    f'{dates[-1].strftime("%d.%m.%Y %H:%M:%S")}')
+
     time_period.grid(column = 0, row = 2, sticky = 'nw')
 
-    time_step = tk.Label(window, anchor = 'nw', text = f"Шаг по времени: {dates[1] - dates[0]}" )
+    time_step = tk.Label(window, anchor = 'nw', 
+                        text = f'Шаг по времени: {dates[1] - dates[0]}')
     time_step.grid(column = 0, row = 3, sticky = 'nw')
 
 
 def get_header():
 
-    combo['values'] = header_row[1:-2]
+    combo['values'] = header_row[1:-1]
 
 
 def graph_builder(code):
@@ -77,9 +169,12 @@ def graph_builder(code):
         header_row = next(reader)
         kks_ind = header_row.index(code)
 
-
         for row in reader:
-            current_date = datetime.strptime(row[0], '%d.%m.%Y %H:%M:%S.%f ')
+            try:
+                current_date = datetime.strptime(row[0], 
+                                        '%d.%m.%Y %H:%M:%S.%f ')
+            except ValueError:
+                current_date = datetime.strptime(row[0], '%d.%m.%y %H:%M:%S')
             dates.append(current_date)
             param = float(row[kks_ind])
             params.append(param)
@@ -97,18 +192,61 @@ def graph_builder(code):
     plt.show()
 
 
+def cascade_graph_builder():
+
+    path_name = filedialog.askdirectory()
+
+    f = open(file_name)
+
+    for ind in range(1,len(header_row) - 1):
+
+        reader = csv.reader(f)
+        next(reader)
+
+        params, dates = [],[]
+
+        for row in reader:
+            try:
+                current_date = datetime.strptime(row[0], 
+                                        '%d.%m.%Y %H:%M:%S.%f ')
+            except ValueError:
+                current_date = datetime.strptime(row[0], '%d.%m.%y %H:%M:%S')
+            dates.append(current_date)
+            param = float(row[ind])
+            params.append(param)
+
+        f.seek(0)
+
+        plt.style.use('seaborn')
+        fig, ax = plt.subplots()
+        ax.plot(dates, params, c='red')
+
+        plt.title("", fontsize = 24)
+        plt.xlabel(header_row[0], fontsize = 16)
+        fig.autofmt_xdate()
+        plt.ylabel(header_row[ind], fontsize = 16)
+        plt.tick_params(axis = 'both', which = 'major', labelsize = 16)
+
+
+        plt.savefig(path_name +  '/' + header_row[ind] + '.png', 
+                                            dpi = 200, bbox_inches ='tight')
+
+    tk.messagebox.showinfo('Сообщение', 'Все графики построены!')
+    f.close()
 
 
 
 window = tk.Tk()
 window.title("Архивы")
-window.geometry('600x150')
+window.geometry('600x200')
 
 file_menu = Menu(window)
 conv = Menu(file_menu, tearoff = 0)
-conv.add_command(label = 'Конвертировать .txt файл в .csv', font = ('', 10), command = file_converter)
+conv.add_command(label = 'Конвертировать файлы архивов СВРК(СВБУ)', 
+                    font = ('', 10), command = file_converter)
 conv.add_separator()
-conv.add_command(label = 'Открыть .csv файл', font = ('', 10), command = file_opener)
+conv.add_command(label = 'Открыть .csv файл', font = ('', 10), 
+                    command = file_opener)
 file_menu.add_cascade(label = 'Файл', menu = conv)
 window.config(menu = file_menu)
 
@@ -124,13 +262,19 @@ time_period.grid(column = 0, row = 2, sticky = 'nw')
 time_step = tk.Label(window, anchor = 'nw', text = "Шаг по времени:" )
 time_step.grid(column = 0, row = 3, sticky = 'nw')
 
-combo = ttk.Combobox(window, values = [], postcommand = get_header, state = "readonly")
+combo = ttk.Combobox(window, values = [], postcommand = get_header, 
+                            state = "readonly")
 combo.grid(column = 0, row = 4,sticky = 'nw')
 
 
 
-graph_button = tk.Button(window, text = 'Постротить график', command = lambda: graph_builder(combo.get()))
+graph_button = tk.Button(window, text = 'Постротить график', 
+                            command = lambda: graph_builder(combo.get()))
 graph_button.grid(column = 0, row = 5,sticky = 'nw')
+
+cascade_button = tk.Button(window, text = 'Построить графики всех параметров', 
+                            command = cascade_graph_builder)
+cascade_button.grid(column = 0, row = 6,sticky = 'nw')
 
 
 window.mainloop()
